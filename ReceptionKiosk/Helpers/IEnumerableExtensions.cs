139bee280cccdc97337f070709112a1575de30c4
Kevin Rottsieper <Kevin.Rottsieper@de.issworld.com>
2017-05-30 15:08:33 +0000

    + Load PersonGroups

---
+++ ReceptionKiosk/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReceptionKiosk.Helpers
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// ForEach Implementierung für IEnumerable
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="enumeration">Enumeration</param>
+        /// <param name="action">Action</param>
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
