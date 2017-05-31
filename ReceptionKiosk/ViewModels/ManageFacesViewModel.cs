c65d4cfcfc93ca6aed748d2007fd2e8d93a73294
Kevin Rottsieper <Kevin.Rottsieper@de.issworld.com>
2017-05-31 14:27:53 +0000

    loading groups, persons and count of trained faces

--- ReceptionKiosk/ViewModels/ManageFacesViewModel.cs
+++ ReceptionKiosk/ViewModels/ManageFacesViewModel.cs
@@ -1,19 +1,34 @@
 using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Face.Contract;
-
 using ReceptionKiosk.Helpers;
-
+using ReceptionKiosk.Services;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
-using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
 
 namespace ReceptionKiosk.ViewModels
 {
     public class ManageFacesViewModel : Observable
     {
-        //TODO: Abändern
-        private FaceServiceClient fsc = new FaceServiceClient("XXXXXXXXXXXXXXXX", "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
+        /// <summary>
+        /// Settings
+        /// </summary>
+        private static APISettingsService apiSettingsService = new APISettingsService();
+
+        //TODO: Später in einen Services auspacken
+        private FaceServiceClient fsc;
+
+        #region Commands
+
+        public ICommand AddGroupCommand { get; private set; }
+
+        #endregion //Commands
+
+        #region Collections
 
         /// <summary>
         /// Liste aller verfügbaren Gruppen
@@ -25,6 +40,15 @@ namespace ReceptionKiosk.ViewModels
         /// </summary>
         public ObservableCollection<Person> Persons { get; set; }
 
+        /// <summary>
+        /// Alle Bilder zu einer Person
+        /// </summary>
+        public ObservableCollection<PersonFace> PersonFaces { get; set; }
+
+        #endregion //Collections
+
+        #region Properties
+
         private bool _isLoading;
         public bool IsLoading
         {
@@ -32,33 +56,141 @@ namespace ReceptionKiosk.ViewModels
             set { Set(ref _isLoading, value); }
         }
 
-        public ICommand LoadPersonsCommand { get; private set; }
+        //TODO: Später an UI_Element binden
+        private PersonGroup _selectedPersonGroup;
+        public PersonGroup SelectedPersonGroup
+        {
+            get { return _selectedPersonGroup; }
+            set { Set(ref _selectedPersonGroup, value); }
+        }
+
+        //TODO: Später an UI_Element binden
+        private Person _selectedPerson;
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set { Set(ref _selectedPerson, value); }
+        }
+
+        private string _countOfFaces;
+        public string CountOfFaces
+        {
+            get { return _countOfFaces; }
+            set { Set(ref _countOfFaces, value); }
+        }
+
+        private string _groupToAdd;
+        public string GroupToAdd
+        {
+            get { return _groupToAdd; }
+            set { Set(ref _groupToAdd, value); }
+        }
+
+
+        #endregion //Properties
 
         /// <summary>
         /// CTOR
         /// </summary>
         public ManageFacesViewModel()
         {
+            //ObservableCollection initialisieren
             PersonGroups = new ObservableCollection<PersonGroup>();
-            LoadPersonsCommand = new RelayCommand(ExecuteLoadPersons, CanExecuteLoadPersons);
+            Persons = new ObservableCollection<Person>();
+            PersonFaces = new ObservableCollection<PersonFace>();
 
-            Initialize();
+            //Command initialisieren
+            AddGroupCommand = new RelayCommand(async () => await ExecuteAddGroupCommandAsync(), CanExecuteAddGroupCommand);
         }
 
-        private void ExecuteLoadPersons()
+        #region AddGroupCommand
+        private async Task ExecuteAddGroupCommandAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (string.IsNullOrEmpty(GroupToAdd))
+                    throw new ArgumentNullException(nameof(GroupToAdd), "Please enter a group name.");
+
+                await fsc.CreatePersonGroupAsync(Guid.NewGuid().ToString(), GroupToAdd);
+                await (new MessageDialog("Your group is added.")).ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog(ex.Message, "Fehler");
+                await dialog.ShowAsync();
+            }
         }
 
-        private bool CanExecuteLoadPersons()
+        private bool CanExecuteAddGroupCommand()
         {
-            throw new NotImplementedException();
+            return !string.IsNullOrEmpty(GroupToAdd);
+        }
+        #endregion //AddGroupCommand
+
+        public async void OnPersonGroupChanged(object sender, SelectionChangedEventArgs args)
+        {
+            try
+            {
+                var listBoxPersonGroup = sender as ListBox;
+
+                if (listBoxPersonGroup?.SelectedItem != null)
+                {
+                    if (listBoxPersonGroup.SelectedItem is PersonGroup)
+                    {
+                        SelectedPersonGroup = listBoxPersonGroup.SelectedItem as PersonGroup;
+                        if (SelectedPersonGroup != null)
+                        {
+                            var persons = await fsc.ListPersonsAsync(SelectedPersonGroup.PersonGroupId);
+                            persons.ForEach(p => Persons.Add(p));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog(ex.Message, "Fehler");
+                await dialog.ShowAsync();
+            }
+        }
+
+        public async void OnPersonChanged(object sender, SelectionChangedEventArgs args)
+        {
+            try
+            {
+                var listBoxPerson = sender as ListBox;
+
+                if (listBoxPerson?.SelectedItem != null)
+                {
+                    if (listBoxPerson.SelectedItem is Person)
+                    {
+                        SelectedPerson = listBoxPerson.SelectedItem as Person;
+                        if (SelectedPerson != null)
+                        {
+                            ////TODO: Muss noch implementiert werden
+                            //foreach (Guid faceId in SelectedPerson.PersistedFaceIds)
+                            //{
+                            //    var personFace = await fsc.GetPersonFaceAsync(SelectedPersonGroup.PersonGroupId, SelectedPerson.PersonId, faceId);
+                            //    PersonFaces.Add(personFace);
+                            //}
+                            CountOfFaces = $"{SelectedPerson.PersistedFaceIds.Count()} trained faces.";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var dialog = new MessageDialog(ex.Message, "Fehler");
+                await dialog.ShowAsync();
+            }
         }
 
         public async void Initialize()
         {
             IsLoading = true;
-            
+
+            await apiSettingsService.LoadAPIKeysFromSettingsAsync();
+            fsc = new FaceServiceClient(apiSettingsService.FaceApiKey, "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
+
             var personGroups = await fsc.ListPersonGroupsAsync();
             personGroups.OrderBy(pg => pg.Name);
             personGroups.ForEach(pg => PersonGroups.Add(pg));
--- ReceptionKiosk/Views/ManageFacesPage.xaml.cs
+++ ReceptionKiosk/Views/ManageFacesPage.xaml.cs
@@ -1,15 +1,22 @@
 using ReceptionKiosk.ViewModels;
 
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace ReceptionKiosk.Views
 {
     public sealed partial class ManageFacesPage : Page
     {
         public ManageFacesViewModel ViewModel { get; } = new ManageFacesViewModel();
+
         public ManageFacesPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel.Initialize();
         }
     }
 }
