728aad31079ed727c4a4c43476772ccefc3b9c07
Malte Lantin <n0tec@outlook.com>
2017-06-12 20:21:02 +0200

    Add a first version of the recognition page with offline detection in camera control

--- ReceptionKiosk/Views/MainPage.xaml.cs
+++ ReceptionKiosk/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 using ReceptionKiosk.ViewModels;
-
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -7,15 +8,42 @@ namespace ReceptionKiosk.Views
 {
     public sealed partial class MainPage : Page
     {
-        public MainViewModel ViewModel { get; } = new MainViewModel();
+        public MainViewModel ViewModel { get; }
+
         public MainPage()
-        {
+        {            
             InitializeComponent();
+            Application.Current.Suspending += Application_Suspending;
+            Application.Current.EnteredBackground += Current_EnteredBackground;
+            ViewModel = new MainViewModel(cameraControl);
         }
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             await ViewModel.InitializeAsync();
+        }
+
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            ViewModel.stopLoop();
+            await cameraControl.CleanupCameraAsync();
+        }
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(AddFacePage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await cameraControl.CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+
+        private void Current_EnteredBackground(object sender, EnteredBackgroundEventArgs e)
+        {
+            cameraControl.Reset();
         }
     }
 }
